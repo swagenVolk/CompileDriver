d0c97eab00e9c854636b78f61fdee13d25ab32fd
Mike Volk <swagen@comcast.net>
2025-02-05 14:50:22 -0800

    Added CMake related files.

---
+++ locale_strings.h
@@ -0,0 +1,35 @@
+/* ****************************************************************************
+ * Header file to contain strings and other #defines that allows for localization.
+ * Highly doubtful this will be *actually* necessary, but it's a good practice
+ * regardless
+ * ***************************************************************************/
+#ifndef LOCALE_STRINGS_H
+#define LOCALE_STRINGS_H
+
+
+/* *************** <RESERVED WORDS> *************** */
+#define FALSE_RESERVED_WORD           L"false"
+#define TRUE_RESERVED_WORD            L"true"
+#define IF_RESERVED_WORD              L"if"
+#define ELSE_RESERVED_WORD            L"else"
+#define WHILE_RESERVED_WORD           L"while"
+#define FOR_RESERVED_WORD             L"for"
+#define BREAK_RESERVED_WORD           L"break"
+#define RETURN_RESERVED_WORD          L"return"
+#define VOID_RESERVED_WORD            L"void"
+
+/* *************** <DATA TYPES> *************** */
+#define DATA_TYPE_UINT8               L"uint8"
+#define DATA_TYPE_UINT16              L"uint16"
+#define DATA_TYPE_UINT32              L"uint32"
+#define DATA_TYPE_UINT64              L"uint64"
+#define DATA_TYPE_INT8                L"int8"
+#define DATA_TYPE_INT16               L"int16"
+#define DATA_TYPE_INT32               L"int32"
+#define DATA_TYPE_INT64               L"int64"
+#define DATA_TYPE_STRING              L"string"
+#define DATA_TYPE_DATETIME            L"datetime"
+#define DATA_TYPE_DOUBLE              L"double"
+#define DATA_TYPE_BOOL                L"bool"
+
+#endif
