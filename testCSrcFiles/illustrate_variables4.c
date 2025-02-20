301796f4750653abdbc65285e609dba0e57b1dc3
Mike Volk <swagen@comcast.net>
2025-02-20 14:37:36 -0800

    Refined output of ILLUSTRATIVE files.

--- testCSrcFiles/illustrate_variables4.c
+++ testCSrcFiles/illustrate_variables4.c
@@ -1,6 +1,8 @@
+// To keep the level of illustrative output reasonable, expressions inside variable declarations (ie on line below) are not logged
 int16 one = 1, two = one + one, three = two + one, four = three + one, five = three + two, six = three * two, seven = four + three, eight = four * two, nine = three * three;
 
 int32 result;
 
+// The expression below will have illustrative logging.
 result = ((one + two) + three) * (four + six) + (six * one << five <  eight * four ? three : four);
 //           (3 + 3) * 10 + four
